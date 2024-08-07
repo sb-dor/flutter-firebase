13523bf3099c7a354940d78d38e0e714a50fa783
sb-dor <avaz.shams.2002@gmail.com>
2024-08-07 21:41:00 +0500

    added firebase auth files

---
+++ lib/core/firebase_helpers/firebase_auth/firebase_facebook_auth/firebase_facebook_auth_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirebaseFacebookAuthPage extends StatefulWidget {
+  const FirebaseFacebookAuthPage({super.key});
+
+  @override
+  State<FirebaseFacebookAuthPage> createState() => _FirebaseFacebookAuthPageState();
+}
+
+class _FirebaseFacebookAuthPageState extends State<FirebaseFacebookAuthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
