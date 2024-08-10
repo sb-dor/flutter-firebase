d7fc8f9b2cd3951860b4c1fea768f1373a32f305
sb-dor <avaz.shams.2002@gmail.com>
2024-08-10 12:18:00 +0500

    added config files for ios

---
+++ lib/core/firebase_helpers/firebase_auth/firebase_apple_auth/firebase_apple_auth_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirebaseAppleAuthPage extends StatefulWidget {
+  const FirebaseAppleAuthPage({super.key});
+
+  @override
+  State<FirebaseAppleAuthPage> createState() => _FirebaseAppleAuthPageState();
+}
+
+class _FirebaseAppleAuthPageState extends State<FirebaseAppleAuthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,6 +18,7 @@ import flutter_secure_storage_macos
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
+import sign_in_with_apple
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -34,5 +35,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
