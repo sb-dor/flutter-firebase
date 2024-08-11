c4000ee95a8835066f6118e359a7231899675b45
sb-dor <avaz.shams.2002@gmail.com>
2024-08-11 12:17:48 +0500

    uploaded

---
+++ lib/core/firebase_helpers/firebase_messaging/other_functionalities/local_notification_helper.dart
@@ -0,0 +1,28 @@
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationHelper {
+  // static final _notification = FlutterLocalNotificationsPlugin();
+
+  //all initial for any devices
+  static Future<void> initLocalNotification() async {
+    // var androidInit = const AndroidInitializationSettings('mipmap/ic_launcher');
+    // var iosAndMacInit = const DarwinInitializationSettings();
+    // // var linuxInit = const LinuxInitializationSettings(defaultActionName: 'any');
+    // var initialSettings = InitializationSettings(
+    //     android: androidInit, iOS: iosAndMacInit, macOS: iosAndMacInit);
+    //
+    // await _notification.initialize(initialSettings);
+  }
+
+  //call this fun for showing local notification
+  static Future<void> showNotification(
+      {int id = 0, required String title, required String body, var playLoad}) async {
+    // AndroidNotificationDetails androidNotificationDetails = AndroidNotificationDetails(
+    //     "any_id", "channel_name",
+    //     playSound: true, importance: Importance.max, priority: Priority.high);
+    //
+    // var notDetails =
+    //     NotificationDetails(android: androidNotificationDetails, iOS: DarwinNotificationDetails());
+    // await _notification.show(id, title, body, notDetails);
+  }
+}
